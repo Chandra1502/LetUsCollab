0de3e9a42120e3020ddd8de4c7663986b81bd6b9
Chandra1502 <chandu.pcv@gmail.com>
2017-08-28 21:36:29 +0530

    Controllers and Services of all the modules

---
+++ cofrontend/WebContent/c_home/HomeController.js
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+
+
+app.controller('HomeController', function($scope) {
+	
+	console.log("This is from HomeController...");
+	
+	$scope.message="Message from Home Controller"
+	
+	});
+
+
+
---
+++ cofrontend/WebContent/c_user/LogoutController.js
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+
+
+
+app.controller('LogoutController', function($scope,$rootScope) {
+	console.log("In LogoutController..")
+       $scope.message = "You successfully loggedout";
+	
+	$rootScope.isLoggedIn="false"
+
+});
+
+
